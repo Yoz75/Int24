fea34e13e037ed12c73d42858a5405afba483902
yoz75 <levtannn@gmail.com>
2023-10-27 16:31:36 +0300

    bug fixed

--- Int24.cs
+++ Int24.cs
@@ -6,9 +6,9 @@ namespace Int24
 
         public const int MAX_VALUE = 8388607;
         public const int MIN_VALUE = -8388608;
-        private byte LowBits = 0;
-        private byte MiddleBits = 0;
-        private byte HighBits = 0;
+        private sbyte LowBits = 0;
+        private sbyte MiddleBits = 0;
+        private sbyte HighBits = 0;
 
 
         public Int24(long value)
@@ -21,23 +21,23 @@ namespace Int24
 
             //В Int24 3 байта, так что берём три последних байта из числа и используем
             //побитовые сдвиги для того, чтобы в каждом поле оказался свой байт.
-            LowBits = (byte)value;
-            MiddleBits = (byte)(value >> 8);
-            HighBits = (byte)(value >> 16);
+            LowBits = (sbyte)value;
+            MiddleBits = (sbyte)(value >> 8);
+            HighBits = (sbyte)(value >> 16);
         }
 
         public int GetIntValue()
         {
             //"Складываем" при помощи побитового или
             //значения из  наших 3 байтов, попутно возвращая их на свои места.
-            return LowBits | MiddleBits << 8 | HighBits << 16;
+            return (LowBits | MiddleBits << 8 | HighBits << 16);
         }
         public override string ToString()
         {
             return GetIntValue().ToString();
         }
 
-        public static implicit operator Int24(byte value) => new Int24(value);
+        public static implicit operator Int24(sbyte value) => new Int24(value);
 
         public static implicit operator Int24(short value) => new Int24(value);
 
